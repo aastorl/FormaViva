188ebc8403b738fb90972e4e06f5dd4e221154b2
aastorl <aastorl79@gmail.com>
2025-04-10 13:55:07 -0300

    Initial Commit

---
+++ FormaViva/FormaVivaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormaVivaApp.swift
+//  FormaViva
+//
+//  Created by Astor Ludueña  on 10/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct FormaVivaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
